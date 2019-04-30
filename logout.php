113907e50cd984de0e0bbf401cdc68859a57334b
BuildTools <unconfigured@null.spigotmc.org>
2019-04-30 13:01:22 -0400

    fix logout

--- logout.php
+++ logout.php
@@ -7,5 +7,5 @@ unset($_SESSION["password"]);
 
 // wait 2 seconds and redirect to account page
 echo 'You have been signed out.';
-header('Refresh: 2; URL = account.php');
+header('Refresh: 2; Location: account.php');
 ?>
