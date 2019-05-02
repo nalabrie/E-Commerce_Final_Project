81eca59760e161c25498953b9ffe36f3e79b64dd
BuildTools <unconfigured@null.spigotmc.org>
2019-05-02 14:43:14 -0400

    added some notes

--- cart.php
+++ cart.php
@@ -8,5 +8,6 @@
 <?php include("./navbar.inc") ?>
 There is nothing to be displayed here until the menu and sign-in is fully functional.
 <!-- TODO: this entire feature -->
+<!-- TODO: remember that the cart is checkboxes holding ITEM_NUM but should be displayed to the user ONLY IF box is checked AND show DESCRIPTION rather than a number and a quantity adjuster is needed -->
 </body>
 </html>
